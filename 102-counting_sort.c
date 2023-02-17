505ade6d99c9e8d78e4386bf30305e5c754a9d33
mohammedshanan <mohammedshanan12@gmail.com>
2023-02-17 15:11:01 +0200

    solved countingsort

---
+++ 102-counting_sort.c
@@ -0,0 +1,40 @@
+#include "sort.h"
+/**
+ * counting_sort - counting sort algo
+ * @array: array to sort
+ * @size: size of array
+ */
+void counting_sort(int *array, size_t size)
+{
+int *counts, *sorted_arr, indx, k = array[0];
+size_t i = 0;
+if (array == NULL || size < 2)
+return;
+for (i = 0; i < size; i++)
+k = array[i] > k ? array[i] : k;
+counts = malloc(sizeof(int) * (k + 1));
+if (counts == NULL)
+return;
+sorted_arr = malloc(sizeof(int) * size);
+if (sorted_arr == NULL)
+{
+free(counts);
+return;
+}
+for (indx = 0; indx < k + 1; indx++)
+counts[indx] = 0;
+for (i = 0; i < size; i++)
+counts[array[i]] += 1;
+for (indx = 0; indx < k; indx++)
+counts[indx + 1] += counts[indx];
+print_array(counts, k + 1);
+for (i = 0; i < size; i++)
+{
+sorted_arr[counts[array[i]] - 1] = array[i];
+counts[array[i]] += 1;
+}
+for (i = 0; i < size; i++)
+array[i] = sorted_arr[i];
+free(sorted_arr);
+free(counts);
+}
