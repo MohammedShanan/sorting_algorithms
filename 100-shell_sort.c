f1e83ed5a5bb4c32789c9ae1b07854735ac72a77
mohammedshanan <mohammedshanan12@gmail.com>
2023-02-17 10:45:30 +0200

    solved shellsort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,21 +1,32 @@
 #include "sort.h"
+/**
+ * shell_sort - sorts shelly like
+ * @array: array to take in
+ * @size: size of array
+ *
+ */
 void shell_sort(int *array, size_t size)
 {
-    size_t gap = 1, i;
-    int tmp;
-    if (array == NULL || size < 2)
-        return;
-    while (gap < size / 3)
-    {
-        gap = gap * 3 + 1;
-    }
-    while (gap)
-    {
-        for (i = gap; i < size; i++)
-        {
-            
-        }
-
-        gap / 2;
-    }
+size_t gap = 1, i, j;
+int tmp;
+if (array == NULL || size < 2)
+return;
+while (gap < size / 3)
+{
+gap = gap * 3 + 1;
+}
+while (gap)
+{
+for (i = gap; i < size; i++)
+{
+tmp = array[i];
+for (j = i; j >= gap && tmp < array[j - gap]; j -= gap)
+{
+array[j] = array[j - gap];
+}
+array[j] = tmp;
+}
+print_array(array, size);
+gap /= 3;
+}
 }
