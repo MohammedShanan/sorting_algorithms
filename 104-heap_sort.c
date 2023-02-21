dd87b6e2cc87de289e9a3655af2f1db2ed8b1ceb
mohammedshanan <mohammedshanan12@gmail.com>
2023-02-22 01:33:20 +0200

    solved heapsort

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,4 +1,59 @@
 #include "sort.h"
-void radix_sort(int *array, size_t size)
+/**
+ * swap - swap two nodes
+ * @a: pointer to the first node
+ * @b: pointer to the second node
+ */
+void swap(int *a, int *b)
 {
+
+int temp = *a;
+
+*a = *b;
+
+*b = temp;
+}
+/**
+ * heap_sort - heap sort algo
+ * @arr: array sort
+ * @N: size of heap
+ * @i: index of root node
+ * @size: size of array
+ */
+void heapify(int *arr, int N, int i, size_t size)
+{
+
+int largest = i;
+int left = 2 * i + 1;
+int right = 2 * i + 2;
+if (left < N && arr[left] > arr[largest])
+
+largest = left;
+if (right < N && arr[right] > arr[largest])
+
+largest = right;
+if (largest != i)
+{
+
+swap(&arr[i], &arr[largest]);
+print_array(arr, size);
+heapify(arr, N, largest, size);
+}
+}
+/**
+ * heap_sort - heap sort algo
+ * @array: array sort
+ * @size: size of array
+ */
+void heap_sort(int *array, size_t size)
+{
+int i;
+for (i = size / 2 - 1; i >= 0; i--)
+heapify(array, size, i, size);
+for (i = size - 1; i >= 0; i--)
+{
+swap(&array[0], &array[i]);
+print_array(array, size);
+heapify(array, i, 0, size);
+}
 }
